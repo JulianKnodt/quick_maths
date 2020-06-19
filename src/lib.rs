c79ef28f5e9490d551dd8afc1494266cd96369d0
julianknodt <julianknodt@gmail.com>
2020-06-19 00:43:50 -0700

    Added inlined docs re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 // Linear algebra modules
 /// Matrix definitions
 pub mod mat;
+#[doc(inline)]
 pub use mat::*;
 
 /// Quaternion definitions
@@ -11,12 +12,15 @@ pub mod quat;
 
 /// Simple Ray definitions
 pub mod ray;
+#[doc(inline)]
 pub use ray::*;
 
 /// Vector definitions
 pub mod vec;
+#[doc(inline)]
 pub use vec::*;
 
 /// Convenience trait to make all floats also debug
 pub mod num;
+#[doc(inline)]
 pub use crate::num::*;
